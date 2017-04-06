bfcde2287ab5bdb84f9663f9a1f7a41149358b4e
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-04-06 18:12:27 +1000

    Rx example is working

--- app/src/main/java/au/csiro/ozatlas/login/LoginActivity.java
+++ app/src/main/java/au/csiro/ozatlas/login/LoginActivity.java
@@ -2,7 +2,13 @@ package au.csiro.ozatlas.login;
 
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.util.Log;
 import android.widget.EditText;
+
+import com.google.gson.Gson;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import javax.inject.Inject;
 
@@ -12,12 +18,19 @@ import au.csiro.ozatlas.rest.RestClient;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by sad038 on 6/4/17.
  */
 
 public class LoginActivity extends BaseActivity {
+    private final String TAG = "LoginActivity";
+
     @BindView(R.id.inputLayoutUsername)
     TextInputLayout inputLayoutUsername;
     @BindView(R.id.editUsername)
@@ -27,11 +40,43 @@ public class LoginActivity extends BaseActivity {
     @BindView(R.id.editPassword)
     EditText editPassword;
 
+    private CompositeDisposable mCompositeDisposable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mCompositeDisposable = new CompositeDisposable();
+        postLogin();
+    }
+
+    private void postLogin(){
+        restClient.getService().register()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(String value) {
+                        Log.d(TAG, "onNext");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete");
+                    }
+                });
     }
 
     @OnClick(R.id.loginButton)
--- app/src/main/java/au/csiro/ozatlas/rest/ApiService.java
+++ app/src/main/java/au/csiro/ozatlas/rest/ApiService.java
@@ -1,8 +1,17 @@
 package au.csiro.ozatlas.rest;
 
+import com.google.gson.Gson;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
 /**
  * Created by sad038 on 5/4/17.
  */
 
 public interface ApiService {
+    @GET("58e5f82c1100003210bfc6a0")
+    Observable<String> register();
 }
