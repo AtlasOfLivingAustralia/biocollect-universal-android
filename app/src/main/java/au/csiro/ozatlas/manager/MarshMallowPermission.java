a6d0b474d26211a0f60b388921cadc46c31692f3
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-04-12 14:00:38 +1000

    MarshMallow permissions

---
+++ app/src/main/java/au/csiro/ozatlas/manager/MarshMallowPermission.java
@@ -0,0 +1,93 @@
+package au.csiro.ozatlas.manager;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+public class MarshMallowPermission {
+
+    public static final int EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 2;
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 3;
+    public static final int LOCATION_PERMISSION_REQUEST_CODE = 4;
+
+    Activity activity;
+    Fragment fragment;
+    Context context;
+
+    public MarshMallowPermission(Activity activity) {
+        this.activity = activity;
+        this.context = activity;
+    }
+
+    public MarshMallowPermission(Fragment fragment) {
+        this.fragment = fragment;
+        this.context = fragment.getContext();
+    }
+
+    public boolean isPermissionGrantedForContacts() {
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public boolean isPermissionGrantedForExternalStorage() {
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public boolean isPermissionGrantedForCamera() {
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public boolean isPermissionGrantedForLocation() {
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public boolean isPermissionGrantedForSMS() {
+        int result = ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void requestPermissionForExternalStorage() {
+        if (this.fragment != null) {
+            fragment.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(activity, "External Storage permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    public void requestPermissionForCamera() {
+        if (this.fragment != null) {
+            fragment.requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CAMERA)) {
+                Toast.makeText(activity, "Camera permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    public void requestPermissionForLocation() {
+        if (this.fragment != null) {
+            fragment.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(activity, "Location permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+}
