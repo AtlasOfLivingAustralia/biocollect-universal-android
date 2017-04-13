509e15a05a34f78867dd3aa8d4afba079fda3718
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-04-13 15:08:30 +1000

    sight models

---
+++ app/src/main/java/au/csiro/ozatlas/model/Sight.java
@@ -0,0 +1,24 @@
+package au.csiro.ozatlas.model;
+
+/**
+ * Created by sad038 on 13/4/17.
+ */
+
+public class Sight {
+    public String dateCreated;
+    public String eventDate;
+    public String family;
+    public String kingdom;
+    public String lastUpdated;
+    public String locality;
+    public String projectId;
+    public String recordedBy;
+    public String scientificName;
+    public String userDisplayName;
+    public String projectName;
+    public String name;
+    public String status;
+    public Double decimalLatitude;
+    public Double decimalLongitude;
+    public Integer individualCount;
+}
---
+++ app/src/main/java/au/csiro/ozatlas/model/SightList.java
@@ -0,0 +1,11 @@
+package au.csiro.ozatlas.model;
+
+import java.util.List;
+
+/**
+ * Created by sad038 on 13/4/17.
+ */
+
+public class SightList {
+    List<Sight> list;
+}
