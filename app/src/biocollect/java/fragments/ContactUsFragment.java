f91ef8f912caac45c0519d5f8271d97fcfce29c2
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-06-14 15:38:02 +1000

    Contact us fragment is added

--- app/src/biocollect/java/fragments/ContactUsFragment.java
+++ app/src/biocollect/java/fragments/ContactUsFragment.java
@@ -7,15 +7,17 @@ import android.view.ViewGroup;
 
 import au.csiro.ozatlas.R;
 import au.csiro.ozatlas.base.BaseFragment;
+import base.BaseMainActivityFragment;
 
 /**
  * Created by sad038 on 8/6/17.
  */
 
-public class ContactUsFragment extends BaseFragment {
+public class ContactUsFragment extends BaseMainActivityFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+        setTitle(getString(R.string.contact_us_title));
         return view;
     }
 }
