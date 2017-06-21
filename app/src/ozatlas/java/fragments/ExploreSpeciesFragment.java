0f3c62ec2a926d1f9cd445ec89a1f1fe7f8ac4a5
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-06-21 16:28:38 +1000

    map is added.

--- app/src/ozatlas/java/fragments/ExploreSpeciesFragment.java
+++ app/src/ozatlas/java/fragments/ExploreSpeciesFragment.java
@@ -5,6 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import au.csiro.ozatlas.R;
 import base.BaseMainActivityFragment;
 import butterknife.ButterKnife;
@@ -13,12 +17,24 @@ import butterknife.ButterKnife;
  * Created by sad038 on 21/6/17.
  */
 
-public class ExploreSpeciesFragment extends BaseMainActivityFragment {
+public class ExploreSpeciesFragment extends BaseMainActivityFragment implements OnMapReadyCallback {
+    private SupportMapFragment mapFragment;
+    private GoogleMap googleMap;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_explore_species, container, false);
         ButterKnife.bind(this, view);
         setTitle(getString(R.string.explore_species_title));
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
     }
 }
