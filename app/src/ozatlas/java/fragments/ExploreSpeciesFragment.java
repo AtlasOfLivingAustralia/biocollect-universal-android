a37fd1aed41aa5456ae8de65888ce915efc0b96c
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-06-27 12:39:57 +1000

    address suggestion is working in explore fragment

--- app/src/ozatlas/java/fragments/ExploreSpeciesFragment.java
+++ app/src/ozatlas/java/fragments/ExploreSpeciesFragment.java
@@ -1,16 +1,26 @@
 package fragments;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,7 +32,14 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import au.csiro.ozatlas.R;
 import au.csiro.ozatlas.manager.MarshMallowPermission;
 import base.BaseMainActivityFragment;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import geocode.Constants;
+import geocode.FetchAddressIntentService;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by sad038 on 21/6/17.
@@ -30,11 +47,21 @@ import butterknife.ButterKnife;
 
 public class ExploreSpeciesFragment extends BaseMainActivityFragment implements OnMapReadyCallback {
     private final int INITIAL_ZOOM = 15;
+    private final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    private final String TAG = "ExploreSpeciesFragment";
 
     private SupportMapFragment mapFragment;
     private GoogleMap googleMap;
     private FusedLocationProviderClient mFusedLocationClient;
     private LatLng currentLocation;
+
+    /**
+     * Receiver registered with this activity to get the response from FetchAddressIntentService.
+     */
+    private AddressResultReceiver mResultReceiver;
+
+    @BindView(R.id.address)
+    EditText address;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,12 +69,25 @@ public class ExploreSpeciesFragment extends BaseMainActivityFragment implements 
         ButterKnife.bind(this, view);
         setTitle(getString(R.string.explore_species_title));
 
+        mResultReceiver = new AddressResultReceiver(new Handler());
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
         mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
 
         return view;
+    }
+
+    @OnClick(R.id.address)
+    void address(){
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY).build(getActivity());
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
     }
 
     private void getLastLocation(){
@@ -63,12 +103,17 @@ public class ExploreSpeciesFragment extends BaseMainActivityFragment implements 
                     public void onSuccess(Location location) {
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
+                            startIntentService(location);
                             currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                            googleMap.addMarker(new MarkerOptions().position(currentLocation));
-                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, INITIAL_ZOOM));
+                            setGoogleMapMarker();
                         }
                     }
                 });
+    }
+
+    private void setGoogleMapMarker(){
+        googleMap.addMarker(new MarkerOptions().position(currentLocation));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, INITIAL_ZOOM));
     }
 
     @Override
@@ -95,6 +140,75 @@ public class ExploreSpeciesFragment extends BaseMainActivityFragment implements 
                     //todo permission denied, boo! Disable the functionality that depends on this permission.
                 }
                 break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(getActivity(), data);
+                currentLocation = place.getLatLng();
+                address.setText(place.getAddress());
+                setGoogleMapMarker();
+
+                Log.i(TAG, "Place: " + place.getName());
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(getActivity(), data);
+                // TODO: Handle the error.
+                Log.i(TAG, status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
+
+    /**
+     * Creates an intent, adds location data to it as an extra, and starts the intent service for
+     * fetching an address.
+     */
+    private void startIntentService(Location location) {
+        // Create an intent for passing to the intent service responsible for fetching the address.
+        Intent intent = new Intent(getActivity(), FetchAddressIntentService.class);
+
+        // Pass the result receiver as an extra to the service.
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+
+        // Pass the location data as an extra to the service.
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, location);
+
+        // Start the service. If the service isn't already running, it is instantiated and started
+        // (creating a process for it if needed); if it is running then it remains running. The
+        // service kills itself automatically once all intents are processed.
+        getActivity().startService(intent);
+    }
+
+    /**
+     * Receiver for data sent from FetchAddressIntentService.
+     */
+    private class AddressResultReceiver extends ResultReceiver {
+        AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        /**
+         *  Receives data sent from FetchAddressIntentService and updates the UI in MainActivity.
+         */
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            // Display the address string or an error message sent from the intent service.
+            address.setText(resultData.getString(Constants.RESULT_DATA_KEY));
+
+            // Show a toast message if an address was found.
+            /*if (resultCode == Constants.SUCCESS_RESULT) {
+                showToast(getString(R.string.address_found));
+            }*/
+
+            // Reset. Enable the Fetch Address button and stop showing the progress bar.
+            //mAddressRequested = false;
         }
     }
 }
