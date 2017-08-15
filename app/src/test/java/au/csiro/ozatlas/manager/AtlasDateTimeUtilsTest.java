e26d302f3756e9b43029a54a0efad126992cd5b1
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-08-15 08:54:31 +0600

    more tests for date utility class

--- app/src/test/java/au/csiro/ozatlas/manager/AtlasDateTimeUtilsTest.java
+++ app/src/test/java/au/csiro/ozatlas/manager/AtlasDateTimeUtilsTest.java
@@ -1,18 +1,37 @@
 package au.csiro.ozatlas.manager;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by sad038 on 15/8/17.
  */
 public class AtlasDateTimeUtilsTest {
-    String dateString1 = "2017-04-18T15:07:40Z";
-    String dateString2 = "2016-02-28T03:03:20Z";
+    private final String dateString1 = "2017-04-18T15:07:40Z";
+    private final String dateString2 = "2016-02-28T03:03:20Z";
 
-    String dateString3 = "2016-03-16 09:45:00";
-    String dateString4 = "2000-04-10 13:45:00";
+    private final String dateString3 = "2016-03-16 09:45:00";
+    private final String dateString4 = "2000-04-10 13:45:00";
+
+    private Calendar calendar;
+
+    @Before
+    public void init() {
+        calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2017);
+        calendar.set(Calendar.MONTH, 5);
+        calendar.set(Calendar.DAY_OF_MONTH, 30);
+        calendar.set(Calendar.HOUR, 10);
+        calendar.set(Calendar.MINUTE, 49);
+        calendar.set(Calendar.SECOND, 30);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
 
     @Test
     public void getFormattedDayTime() throws Exception {
@@ -32,28 +51,16 @@ public class AtlasDateTimeUtilsTest {
     }
 
     @Test
-    public void getFormattedDayTime2() throws Exception {
-
-    }
-
-    @Test
     public void getDateFromString() throws Exception {
-
-    }
-
-    @Test
-    public void getDateFromString1() throws Exception {
-
+        Date date = calendar.getTime();
+        assertEquals(date, AtlasDateTimeUtils.getDateFromString("2017-06-30 10:49:30", "yyyy-MM-dd HH:mm:ss"));
+        assertNotEquals(date, AtlasDateTimeUtils.getDateFromString("2017-05-30 10:49:30", "yyyy-MM-dd HH:mm:ss"));
     }
 
     @Test
     public void getStringFromDate() throws Exception {
-
-    }
-
-    @Test
-    public void getStringFromDate1() throws Exception {
-
+        assertEquals("2017-06-30 10:49:30", AtlasDateTimeUtils.getStringFromDate(calendar.getTime(), "yyyy-MM-dd HH:mm:ss"));
+        assertNotEquals("2017-05-30 10:49:30", AtlasDateTimeUtils.getStringFromDate(calendar.getTime(), "yyyy-MM-dd HH:mm:ss"));
     }
 
 }
