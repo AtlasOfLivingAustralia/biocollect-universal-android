b99f5a9cb5bb6155f377a8036639d5f077f75e42
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-09-26 14:24:20 +1000

    URL changes

--- app/src/biocollect/java/rest/BioCollectApiService.java
+++ app/src/biocollect/java/rest/BioCollectApiService.java
@@ -15,7 +15,7 @@ import retrofit2.http.Query;
  */
 
 public interface BioCollectApiService {
-    @GET("/ws/project/search?fq=isContributingDataToAla:T&initiator=biocollect&sort=nameSort")
+    @GET("/ws/project/search?fq=isExternal:F&initiator=biocollect&sort=nameSort")
     Observable<ProjectList> getProjects(@Query("initiator") String initiator, @Query("max") Integer max, @Query("offset") Integer offset, @Query("mobile") Boolean mobile, @Query("sort") String sort, @Query("q") String q, @Query("isUserPage") Boolean isUserPage);
 
     @GET("bioActivity/searchProjectActivities")
