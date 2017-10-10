8b7931e4c3e6ef6c309da3c6b49eb7c823622b45
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-10-10 12:39:41 +1100

    views for setting fragment and trackers fragment of bilby blitz

--- app/src/bilbyblitz/java/fragments/trackers/TrackersFragment.java
+++ app/src/bilbyblitz/java/fragments/trackers/TrackersFragment.java
@@ -1,10 +1,11 @@
-package fragments;
+package fragments.trackers;
 
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import au.csiro.ozatlas.R;
@@ -16,19 +17,17 @@ import butterknife.ButterKnife;
  * Created by sad038 on 19/9/17.
  */
 
-public class SettingFragment extends BaseMainActivityFragment {
-    @BindView(R.id.projectSpinner)
-    Spinner projectSpinner;
-    @BindView(R.id.languageSpinner)
-    Spinner languageSpinner;
-    @BindView(R.id.dataShareSwitch)
-    SwitchCompat dataShareSwitch;
-    @BindView(R.id.termsAgreeSwitch)
-    SwitchCompat termsAgreeSwitch;
+public class TrackersFragment extends BaseMainActivityFragment {
+    @BindView(R.id.editOrganisationName)
+    EditText editOrganisationName;
+    @BindView(R.id.editLeadTracker)
+    EditText editLeadTracker;
+    @BindView(R.id.editOtherTracker)
+    EditText editOtherTracker;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_homepage, container, false);
+        View view = inflater.inflate(R.layout.fragment_trackers, container, false);
         setTitle(getString(R.string.setting));
         ButterKnife.bind(this, view);
 
