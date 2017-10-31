622d5fc01e323208c1bcbccaf44344b0cc879d3d
Sadat, Sadat (NCMI, Clayton) <Sadat.Sadat@csiro.au>
2017-10-31 11:20:58 +1100

    changes of theme color.

--- app/src/bilbyblitz/java/fragments/offline_species/ExploreSpeciesListFragment.java
+++ app/src/bilbyblitz/java/fragments/offline_species/ExploreSpeciesListFragment.java
@@ -1,6 +1,7 @@
 package fragments.offline_species;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -13,6 +14,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -282,8 +286,10 @@ public class ExploreSpeciesListFragment extends BaseListWithRefreshFragment {
 
                 Integer icon = map.get(group.name);
                 if (icon != null) {
+                    speciesGroupViewHolders.icon.clearColorFilter();
                     speciesGroupViewHolders.icon.setImageResource(icon);
                 } else {
+                    speciesGroupViewHolders.icon.setColorFilter(Color.WHITE);
                     speciesGroupViewHolders.icon.setImageResource(R.drawable.no_image_available);
                 }
             }
@@ -329,7 +335,7 @@ public class ExploreSpeciesListFragment extends BaseListWithRefreshFragment {
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
             if (holder instanceof SpeciesAnimalViewHolders) {
-                SpeciesAnimalViewHolders speciesAnimalViewHolders = (SpeciesAnimalViewHolders) holder;
+                final SpeciesAnimalViewHolders speciesAnimalViewHolders = (SpeciesAnimalViewHolders) holder;
                 ExploreAnimal animal = exploreAnimals.get(position);
                 speciesAnimalViewHolders.name.setText(animal.name);
                 speciesAnimalViewHolders.count.setText(getString(R.string.species_count, animal.count));
@@ -338,6 +344,19 @@ public class ExploreSpeciesListFragment extends BaseListWithRefreshFragment {
                         .load(getString(R.string.explore_image_url, animal.guid))
                         .placeholder(R.drawable.no_image_available)
                         .crossFade()
+                        .listener(new RequestListener<String, GlideDrawable>() {
+                            @Override
+                            public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                                speciesAnimalViewHolders.icon.setColorFilter(Color.WHITE);
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                                speciesAnimalViewHolders.icon.clearColorFilter();
+                                return false;
+                            }
+                        })
                         .into(speciesAnimalViewHolders.icon);
             }
         }
