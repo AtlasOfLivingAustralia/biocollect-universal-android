a7c157d505805a29fccaacc775a6cfc5ee571184
Simon Bear <simon.bear@csiro.au>
2019-10-04 17:36:32 +1000

    Add additional logout test steps

--- app/src/androidTest/java/activity/MainActivityTest.java
+++ app/src/androidTest/java/activity/MainActivityTest.java
@@ -6,21 +6,31 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import au.csiro.ozatlas.R;
+import au.csiro.ozatlas.activity.LoginActivity;
 import au.csiro.ozatlas.manager.AtlasSharedPreferenceManager;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isEmptyString;
 
 public class MainActivityTest {
 
@@ -36,10 +46,11 @@ public class MainActivityTest {
         Context targetContext = getInstrumentation().getTargetContext();
         preferencesEditor = PreferenceManager.getDefaultSharedPreferences(targetContext).edit();
         aspm = new AtlasSharedPreferenceManager(targetContext);
+        Intents.init();
     }
 
     @Test
-    public void testSettings() {
+    public void testSettings() throws InterruptedException {
 
         aspm.writeUserId("test@example.org");
         aspm.writeUsername("test@example.org");
@@ -49,14 +60,43 @@ public class MainActivityTest {
         // Launch activity
         mActivityRule.launchActivity(new Intent());
 
+        // open drawer
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
 
+        // navigate to settings
         onView(withId(R.id.nav_view))
                 .perform(NavigationViewActions.navigateTo(R.id.nav_setting));
 
-
+        // check logout setting is displayed
         onView(withId(R.id.logout_settings_button)).check(matches(isDisplayed()));
+        // check logout small text has user's username
         onView(withId(R.id.logout_settings_small_text)).check(matches(withText("You're currently logged in as test@example.org")));
+        // click logout
+        onView(withId(R.id.logout_settings_button)).perform(click());
 
+        // wait for dialog TODO work around having to do this somehow?!?
+
+        final long SLEEP_TIME = 1000;
+
+        TimeUnit.MILLISECONDS.sleep(SLEEP_TIME);
+
+        // check dialog with logout text is displayed
+        onView(withText(R.string.logout_message)).check(matches(isDisplayed()));
+
+        // check click cancel doesn't do anything
+        onView(withId(android.R.id.button2)).perform(click());
+        TimeUnit.MILLISECONDS.sleep(SLEEP_TIME);
+        onView(withText(R.string.logout_message)).check(doesNotExist());
+        onView(withId(R.id.logout_settings_button)).check(matches(isDisplayed()));
+
+        // click logout
+        onView(withId(R.id.logout_settings_button)).perform(click());
+        TimeUnit.MILLISECONDS.sleep(SLEEP_TIME);
+        // click ok
+        onView(withId(android.R.id.button1)).perform(click());
+
+        assertThat(aspm.getUserId(), isEmptyString());
+        assertThat(aspm.getAuthKey(), isEmptyString());
+        intended(hasComponent(LoginActivity.class.getName()));
     }
 }
