799b84ea297f3e7cf271a0c033a9877bb3e4c70f
Simon Bear <simon.bear@csiro.au>
2019-10-21 12:40:02 +1100

    Add password for tracks app logout (#31)  - Add password entry to logout confirmation dialog  - Ensure device has internet connection before allowing logout

--- app/src/main/java/au/csiro/ozatlas/manager/AtlasDialogManager.java
+++ app/src/main/java/au/csiro/ozatlas/manager/AtlasDialogManager.java
@@ -1,8 +1,24 @@
 package au.csiro.ozatlas.manager;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.annotation.IdRes;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.function.Function;
+
+import au.csiro.ozatlas.R;
+
+import static android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE;
 
 /**
  * Created by sad038 on 3/5/17.
@@ -12,6 +28,8 @@ import android.support.v7.app.AlertDialog;
  * A class to create Alert Dialogs
  */
 public class AtlasDialogManager {
+
+    public static String TAG = "AtlasDialogManager";
 
     /**
      * @param context
@@ -123,5 +141,47 @@ public class AtlasDialogManager {
         builder.setMessage(message).setPositiveButton(positiveButtonText, null);
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    public static void messageBox(Context context, String title, String message, @StringRes int editTextHint, @StringRes int positiveButtonLabel, @StringRes int negativeButtonLabel, StringCallback onClickListener) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title);
+        final View view = LayoutInflater.from(context).inflate(R.layout.dialog_text_input, null);
+        final EditText input = view.findViewById(R.id.dialog_text_input_input);
+        final TextView messageView = view.findViewById(R.id.dialog_text_input_message);
+        messageView.setText(message);
+        builder.setView(view);
+        builder.setPositiveButton(positiveButtonLabel, (dialog, which) -> {
+            onClickListener.apply(input.getText());
+        });
+        builder.setNegativeButton(negativeButtonLabel, ((dialog, which) -> {
+            InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+        }));
+        final Dialog dialog = builder.create();
+
+        input.setHint(editTextHint);
+
+        input.setOnFocusChangeListener((focusView, focus) -> {
+
+            Log.d(TAG, "EditText input focus changed " + focus);
+            if (focus) {
+                dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+            }
+
+            if (!focus) {
+                InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+            }
+        });
+
+
+        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+        dialog.show();
+
+    }
+
+    public interface StringCallback {
+        void apply(CharSequence value);
     }
 }
